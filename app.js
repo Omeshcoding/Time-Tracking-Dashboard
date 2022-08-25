961f91b0245de6e0ee16fff263ecf3327dd1342e
umesh <umesharma.us11@gmail.com>
2022-08-25 11:48:37 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// // const hours = document.querySelectorAll(".hours")
+ 
+
+
+// function gettime(){
+    
+//     hours.innerText = 10
+// }
+
+
+let myArray = ['a','b', 'c' ,'d', 'e', 'f'];
+
+myArray.splice(3,3);
+
+console.log(myArray);
